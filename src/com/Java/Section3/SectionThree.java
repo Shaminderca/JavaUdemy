0846ae4457ee887e68d5a7724f22187fcd614f1f
shaminderca <sshaminder94@gmail.com>
2020-08-07 17:58:25 -0400

    THIS IS COMMIT BY SHAMI

---
+++ src/com/Java/Section3/SectionThree.java
@@ -0,0 +1,15 @@
+package com.Java.Section3;
+
+public class SectionThree {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println("Hello");
+      
+      int ageofClient =20;
+      boolean isageOver18 = ageofClient == 20 ? true:false;
+      
+       
+	}
+
+}
